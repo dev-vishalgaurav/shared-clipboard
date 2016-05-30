b589579b0e20565b757d0a64ca866e8760d3cf64
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-05-30 03:06:34 -0400

    added tutorial screen,

--- code/mobile/app/src/main/java/com/sharedclipboard/ui/fragment/PreferencesFragment.java
+++ code/mobile/app/src/main/java/com/sharedclipboard/ui/fragment/PreferencesFragment.java
@@ -1,10 +1,13 @@
 package com.sharedclipboard.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import com.sharedclipboard.R;
 import com.sharedclipboard.storage.preferences.PreferenceUtils;
+import com.sharedclipboard.ui.activity.ScreenSlideActivity;
 
 /**
  * Created by Girouard23 on 5/20/16.
@@ -23,5 +26,15 @@ public class PreferencesFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.preferences);
         findPreference("passcode").setTitle(PreferenceUtils.getString(getActivity(),"passcode","Unknown"));
         findPreference(PreferenceUtils.PREF_EMAIL).setTitle(PreferenceUtils.getString(getActivity(),PreferenceUtils.PREF_EMAIL,"sample@sample.com"));
+        findPreference("help").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getActivity(), ScreenSlideActivity.class);
+                intent.putExtra(ScreenSlideActivity.EXTRA_IS_SETTINGS_LAUNCH,true);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 }
